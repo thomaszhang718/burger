eefd98104026023577ab0fd2aabbe21557715915
Thomas Zhang <Thomas Zhang>
2016-09-20 19:14:43 -0400

    Adding working framework. Need to update css and html

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,36 @@
+/*
+Here is where you create all the functions that will do the routing for your app, and the logic of each route.
+*/
+var express = require('express');
+var router = express.Router();
+var burger = require('../models/burger.js');
+
+router.get('/', function(req,res) {
+	res.redirect('/burgers')
+});
+
+router.get('/burgers', function(req,res) {
+	burger.selectAll(function(data){
+		var hbsObject = {burgers : data}
+		console.log(hbsObject)
+		res.render('index', hbsObject);
+	});
+});
+
+router.post('/burgers/create', function(req,res) {
+	burger.insertOne(['burger_name'], [req.body.burger_name], function(data){
+		res.redirect('/burgers')
+	});
+});
+
+router.put('/burgers/update/:id', function(req,res) {
+	var condition = 'id = ' + req.params.id;
+
+	console.log('condition', condition);
+
+	burger.updateOne({'devoured' : req.body.devoured}, condition, function(data){
+		res.redirect('/burgers');
+	});
+});
+
+module.exports = router;
