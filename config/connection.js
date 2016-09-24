f0f1df1a2cc52d01cf856a4bf482677f45156c8c
Thomas Zhang <Thomas Zhang>
2016-09-24 10:39:35 -0400

    Setting up heroku database

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,10 @@ Here is where you make the connection to the database and export and used by the
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     port: 3306,
-    host: 'localhost',
-    user: 'root',
-    password: '123456t',
-    database: 'burgers_db'
+    host: 'sp6xl8zoyvbumaa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'lwjlqjktzrv3momq',
+    password: 'b3w33vnbm65gqlgg',
+    database: 'tmtn7mx9ijapxvf3'
 });
 
 connection.connect(function(err) {
