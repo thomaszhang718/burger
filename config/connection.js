ded2c7df83f02ed5dc40bb3da1d13b1bdd3d32d5
Thomas Zhang <Thomas Zhang>
2016-09-25 14:54:54 -0400

    Fixing setup for JAWSDB and heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,20 @@
 Here is where you make the connection to the database and export and used by the O.R.M.
 */
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    port: 3306,
-    host: 'sp6xl8zoyvbumaa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'lwjlqjktzrv3momq',
-    password: 'b3w33vnbm65gqlgg',
-    database: 'tmtn7mx9ijapxvf3'
-});
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: '123456t',
+        database: 'burgers_db'
+    });
+}
 
 connection.connect(function(err) {
     if (err) {
